fb48f2206b5c1c789aacb8d9b8f0eb9fcbb40995
darshan <mahajandarshan9@gmail.com>
2023-05-06 14:39:36 +0530

    first commit

---
+++ src/main/java/com/nit/crudoperationrepo/CrudOperationRepoApplication.java
@@ -0,0 +1,13 @@
+package com.nit.crudoperationrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudOperationRepoApplication.class, args);
+    }
+
+}
